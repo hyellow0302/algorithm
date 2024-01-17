f96e72d002e0a61c71610963babb5d5dfad4d868
Hyellow0302 <lhi030210@gmail.com>
2024-01-17 16:54:29 +0900

    [프로그래머스] 탐욕법(Greedy) - 큰 수 만들기, 1회차 시간초과.js

---
+++ 프로그래머스/makeBignumber.js
@@ -0,0 +1,24 @@
+function solution(number, k) {
+  var answer = "";
+  let arr = [];
+  let returnLen = number.length - k;
+  let start = 0;
+  let end = 0;
+  let max = 0;
+
+  number = number.split("");
+  console.log(number);
+
+  for (let i = 0; i < returnLen; i++) {
+    end = number.length - (returnLen - i);
+    arr = number.slice(0, end + 1);
+    console.log(end, arr, number);
+
+    max = Math.max(...arr);
+    number.splice(0, number.indexOf(max.toString()) + 1);
+
+    answer += max.toString();
+  }
+
+  return answer;
+}
