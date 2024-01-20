4d1bceb423f0bd7baa023b171d502d5c473772e2
Hyellow0302 <lhi030210@gmail.com>
2024-01-20 23:38:03 +0900

    [프로그래머스] 피보나치 수.js

---
+++ 프로그래머스/fibonacci_number.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let answer = 0;
+  let a = 0;
+  let b = 1;
+
+  for (let i = 2; i <= n; i++) {
+    answer = (a + b) % 1234567;
+    a = b;
+    b = answer;
+  }
+
+  return answer;
+}
